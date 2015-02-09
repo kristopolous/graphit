b87a79a4fb0073ccab306b9779caa947b91f8b60
Chris McKenzie <kristopolous@yahoo.com>
2015-02-09 05:50:18 -0800

    cleanup

--- graphit.cpp
+++ graphit.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <algorithm>
 #include <locale>
 #include <stdio.h>
 #include <unistd.h>
 #define PI 3.14159265 
+
 using namespace std;
 class Sparkline {
     public:
-        Sparkline(std::wstring &cs) : charset( cs ){ }
+        Sparkline(wstring &cs) : charset( cs ){ }
         virtual ~Sparkline(){ }
  
-        void print(std::vector<float> data, 
+        void print(
+            vector<float> data, 
             int width, int height,
             float min, float max
         ) {
+          int m_charlen = charset.size() - 1;
+
           int row, col;
           float delta = (max - min);
+          float step = (float)data.size() / width;
 
           // first we reduce the number of items down to the number
           // of things we are going to graph. (the width).
@@ -27,15 +31,14 @@ class Sparkline {
           // going the other way.
 
           // we are fitting x units into y space
-          float step = (float)data.size() / (float)width;
           float nextval, lhs;
-          float iy = 0, pos = 0.0, goal = step;
-          int ix = 0;
+          float iy = 0.0, pos = 0.0, goal = step;
+          int ix;
 
           vector<float> interpolate;
-          nextval = 0.0;
 
-          for(;;) {
+          for(ix = 0; ix < data.size();) {
+            nextval = 0.0;
 
             for(; (iy + 1) < goal; iy++) {
               nextval += data[ix];
@@ -50,7 +53,7 @@ class Sparkline {
                 nextval += lhs * data[ix];
 
                 ix++;
-                nextval += (goal - iy -lhs) * data[ix];
+                nextval += (goal - iy - lhs) * data[ix];
               } else {
                 // ix is already where we want it to be.
                 nextval += (goal - iy) * data[ix];
@@ -61,11 +64,6 @@ class Sparkline {
             interpolate.push_back(nextval / step);
             iy = goal;
             goal += step;
-            nextval = 0.0;
-
-            if(ix >= data.size()) {
-              break;
-            }
           }
 
           // now we have to find out what the graph height should be
@@ -74,15 +72,15 @@ class Sparkline {
           vector<float>::const_iterator it;
           
           for(it = interpolate.begin(); it != interpolate.end(); it++) {
-            toGraph.push_back( (int)(8.0 * height * ((*it - min) / delta)));
+            toGraph.push_back( (int)(m_charlen * height * ((*it - min) / delta)));
           }
 
           // now we output the actual graph one row at a time.
           int tmp, current_floor, current_ceiling;
           for(row = 0; row < height; row++) {
             // if our value is less than this then it's a space.
-            current_floor = (height - row - 1) * 8;
-            current_ceiling = current_floor + 8;
+            current_floor = (height - row - 1) * m_charlen;
+            current_ceiling = current_floor + m_charlen;
 
             for(col = 0; col < width; col++) {
               tmp = toGraph[col];
@@ -90,25 +88,26 @@ class Sparkline {
                 wcout << " ";
               } else {
                 if (tmp >= current_ceiling) {
-                  tmp = 8;
+                  tmp = m_charlen;
                 } else {
-                  tmp %= 8;
+                  tmp %= m_charlen;
                 }
                 wcout << charset[ tmp ];
                 toGraph[col] -= tmp;
               }
             }
 
-            wcout<<endl;
+            wcout << endl;
           }
  
         }
+
     private:
-        std::wstring &charset;
+        wstring &charset;
 };
  
 int main( int argc, char **argv ){
-  std::wstring charset = L" \u2581\u2582\u2583\u2584\u2585\u2586\u2587\u2588";
+  wstring charset = L" \u2581\u2582\u2583\u2584\u2585\u2586\u2587\u2588";
  
     // Mainly just set up utf-8, so wcout won't narrow our characters.
     locale::global(locale("en_US.utf8"));
@@ -122,6 +121,8 @@ int main( int argc, char **argv ){
     vector<float> f;// (tmp, tmp + sizeof(tmp) / sizeof(tmp[0]) ); 
 
     int ix, iy;
+    int hei = 9;
+    float val;
     for(iy = 0; iy < 5; iy++) {
       f.push_back((float)sin (iy*PI/18));
     }
@@ -132,13 +133,20 @@ int main( int argc, char **argv ){
         f.erase(f.begin());
       }
       iy++;
-      f.push_back((float)sin (iy*PI/18));
+      val = sin (iy*PI/18);
+      f.push_back(val);
 
       wcout<<"\033[0;0f";
-      for(ix = 30; ix < 120; ix+=20) {
-      sl.print(f, 
-          ix, 8,
-          -2, 2);
+      hei = 6;
+
+      for(ix = 20; ix <= 110; ix+=30) {
+        sl.print(f, 
+            ix, hei--,
+            -2, 1.5);
+
+        sl.print(f, 
+            ix, (hei + 3) % 6 + 1,
+            -2, 1.5);
       }
       usleep(60000);
     }
