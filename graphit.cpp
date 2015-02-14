7ec38f4092bd8876a22213ac4b7c76db0311a126
Chris McKenzie <kristopolous@yahoo.com>
2015-02-13 17:50:54 -0800

    internal code cleanup

--- graphit.cpp
+++ graphit.cpp
@@ -7,8 +7,151 @@ wstring Graphit::m_charset = L"";
 
 Graphit::Graphit() {
   if(m_charset.length() == 0) {
-    Graphit::useUnicode(true);
+    Graphit::use_unicode(true);
   }
+}
+
+bool Graphit::use_unicode(bool value) {
+  if(value) {
+    m_charset = L" \u2581\u2582\u2583\u2584\u2585\u2586\u2587\u2588";
+  } else {
+    m_charset = L" _.-~'`";
+  }
+}
+
+int Graphit::interpolate(vector<float>&interpolated, vector<float> data, int width ) {
+  float step = (float)data.size() / width;
+
+  // first we reduce the number of items down to the number
+  // of things we are going to graph. (the width).
+  
+  // For our use-case we'll do linear interpolation for going past
+  // or resolution point and one of a variety of methods when
+  // going the other way.
+
+  // we are fitting x units into y space
+  float nextval, lhs;
+  float iy = 0.0, pos = 0.0, goal = step;
+
+  for(int ix = 0; ix < data.size();) {
+    nextval = 0.0;
+
+    for(; (iy + 1) < goal; iy++) {
+      nextval += data[ix];
+      ix++;
+    }
+    // and this point we've gone through all the 
+    // integer values to interpolate ... we may have
+    // a fractional value left.
+    if(goal > iy) {
+      if((int)goal > (int)iy) {
+        lhs = (int)goal - iy;
+        nextval += lhs * data[ix];
+
+        ix++;
+        nextval += (goal - iy - lhs) * data[ix];
+      } else {
+        // ix is already where we want it to be.
+        nextval += (goal - iy) * data[ix];
+      }
+    }
+
+    // this is our interpolated value.
+    interpolated.push_back(nextval / step);
+    iy = goal;
+    goal += step;
+  }
+
+  return 0;
+}
+
+int Graphit::rasterize(
+    vector<float> &rasterized,
+    vector<float> interpolated, 
+    int height, float min, float max) {
+
+  int m_charlen = Graphit::m_charset.size() - 1;
+  float delta = (max - min);
+
+  // now we have to find out what the graph height should be
+  // this is basically value - min / (max - min)
+  vector<float>::const_iterator it;
+  
+  for(it = interpolated.begin(); it != interpolated.end(); it++) {
+    rasterized.push_back( (int)(m_charlen * height * ((*it - min) / delta)));
+  }
+
+  return 0;
+}
+
+int Graphit::plot(
+    vector<wstring> &buffer,
+    vector<float> rasterized, 
+    int width, int height) {
+
+  int m_charlen = m_charset.size() - 1;
+
+  int 
+    row_ix,
+    col_ix,
+    tmp, 
+    current_floor, 
+    current_ceiling;
+
+  wstring col;
+  
+  for(row_ix = 0; row_ix < height; row_ix++) {
+    col.clear();
+
+    // if our value is less than this then it's a space.
+    current_floor = (height - row_ix - 1) * m_charlen;
+    current_ceiling = current_floor + m_charlen;
+
+    for(col_ix = 0; col_ix < width; col_ix++) {
+      tmp = rasterized[col_ix];
+      if(tmp <= current_floor) {
+        tmp = 0;
+      } else if (tmp >= current_ceiling) {
+        tmp = m_charlen;
+      } else {
+        tmp %= m_charlen;
+      }
+      col += m_charset[ tmp ];
+      rasterized[col_ix] -= tmp;
+    }
+    buffer.push_back(col);
+  }
+
+  return 0;
+}
+
+int Graphit::process(
+  vector<wstring> &output,
+  vector<float> data, 
+  int width, int height,
+  float min, float max
+) {
+  int ret = 0;
+
+  vector<float> interpolation;
+  vector<float> toPlot;
+
+  ret = this->interpolate( interpolation, data, width);
+  if(ret) {
+    return ret;
+  }
+
+  ret = this->rasterize( toPlot, interpolation, height, min, max);
+  if(ret) {
+    return ret;
+  }
+
+  ret = this->plot( output, toPlot, width, height);
+  if(ret) {
+    return ret;
+  }
+
+  return ret;
 }
 
 int Graphit::print_curses(
@@ -49,149 +192,6 @@ int Graphit::print_curses(
   }
 
   wrefresh(win); 
-  return ret;
-}
-
-bool Graphit::useUnicode(bool value) {
-  if(value) {
-    m_charset = L" \u2581\u2582\u2583\u2584\u2585\u2586\u2587\u2588";
-  } else {
-    m_charset = L" _.-~'`";
-  }
-}
-
-int Graphit::interpolate(vector<float>&interpolate, vector<float> data, int width ) {
-  float step = (float)data.size() / width;
-
-  // first we reduce the number of items down to the number
-  // of things we are going to graph. (the width).
-  
-  // For our use-case we'll do linear interpolation for going past
-  // or resolution point and one of a variety of methods when
-  // going the other way.
-
-  // we are fitting x units into y space
-  float nextval, lhs;
-  float iy = 0.0, pos = 0.0, goal = step;
-
-  for(int ix = 0; ix < data.size();) {
-    nextval = 0.0;
-
-    for(; (iy + 1) < goal; iy++) {
-      nextval += data[ix];
-      ix++;
-    }
-    // and this point we've gone through all the 
-    // integer values to interpolate ... we may have
-    // a fractional value left.
-    if(goal > iy) {
-      if((int)goal > (int)iy) {
-        lhs = (int)goal - iy;
-        nextval += lhs * data[ix];
-
-        ix++;
-        nextval += (goal - iy - lhs) * data[ix];
-      } else {
-        // ix is already where we want it to be.
-        nextval += (goal - iy) * data[ix];
-      }
-    }
-
-    // this is our interpolated value.
-    interpolate.push_back(nextval / step);
-    iy = goal;
-    goal += step;
-  }
-
-  return 0;
-}
-
-int Graphit::rasterize(
-    vector<float> &toGraph,
-    vector<float> interpolate, 
-    int height, float min, float max) {
-
-  int m_charlen = Graphit::m_charset.size() - 1;
-  float delta = (max - min);
-
-  // now we have to find out what the graph height should be
-  // this is basically value - min / (max - min)
-  vector<float>::const_iterator it;
-  
-  for(it = interpolate.begin(); it != interpolate.end(); it++) {
-    toGraph.push_back( (int)(m_charlen * height * ((*it - min) / delta)));
-  }
-
-  return 0;
-}
-
-int Graphit::plot(
-    vector<wstring> &buffer,
-    vector<float> toPlot, 
-    int width, int height) {
-  int m_charlen = m_charset.size() - 1;
-
-  // now we output the actual graph one row at a time.
-  int 
-    row_ix,
-    col_ix,
-    tmp, 
-    current_floor, 
-    current_ceiling;
-
-  wstring col;
-  
-  for(row_ix = 0; row_ix < height; row_ix++) {
-    col.clear();
-
-    // if our value is less than this then it's a space.
-    current_floor = (height - row_ix - 1) * m_charlen;
-    current_ceiling = current_floor + m_charlen;
-
-    for(col_ix = 0; col_ix < width; col_ix++) {
-      tmp = toPlot[col_ix];
-      if(tmp <= current_floor) {
-        tmp = 0;
-      } else if (tmp >= current_ceiling) {
-        tmp = m_charlen;
-      } else {
-        tmp %= m_charlen;
-      }
-      col += m_charset[ tmp ];
-      toPlot[col_ix] -= tmp;
-    }
-    buffer.push_back(col);
-  }
-
-  return 0;
-}
-
-int Graphit::process(
-  vector<wstring> &output,
-  vector<float> data, 
-  int width, int height,
-  float min, float max
-) {
-  int ret = 0;
-
-  vector<float> interpolation;
-  vector<float> toPlot;
-
-  ret = this->interpolate( interpolation, data, width);
-  if(ret) {
-    return ret;
-  }
-
-  ret = this->rasterize( toPlot, interpolation, height, min, max);
-  if(ret) {
-    return ret;
-  }
-
-  ret = this->plot( output, toPlot, width, height);
-  if(ret) {
-    return ret;
-  }
-
   return ret;
 }
 
--- graphit.h
+++ graphit.h
@@ -10,43 +10,86 @@ class Graphit {
   public:
     Graphit();
 
-    static bool useUnicode(bool value);
+    // Toggles on or off unicode characters
+    static bool use_unicode(bool value);
 
+    // Takes a vector of floats, data and renders 
+    // inside of a curses window, win, plotting between
+    // Y values min and max.
     int print_curses(
         vector<float> data, 
-        WINDOW*win,
-        float min, float max
-    );
+        WINDOW *win,
+        float min, float max);
 
+    // Identical to print_curses but prints the 
+    // results directly to the screen via wcout 
+    // calls.
+    //
+    // Returns 0 on success.
     int print(
         vector<float> data, 
         int width, int height,
-        float min, float max
-    );
+        float min, float max);
 
   private:
 
+    // Process will take a vector of floats,
+    // data and then do an 
+    //
+    //  1 interpolation
+    //  2 rasterization
+    //  3 plot
+    //
+    // of that data with respect to the width
+    // height, min, and max specified. The
+    // results will go into the output.
+    //
+    // Generally speaking this is all that
+    // is needed to interact with directly.
+    //
+    // Returns 0 on success.
     int process(
-      vector<wstring> &output,
-      vector<float> data, 
-      int width, int height,
-      float min, float max
-    );
+        vector<wstring> &output,
+        vector<float> data, 
+        int width, int height,
+        float min, float max);
 
+    // Takes the vector data and does
+    // a linear interpolation of it to
+    // a width, width, into the vector
+    // interpolated.  Returns 0 on success.
     int interpolate(
-        vector<float> &interpolate, 
+        vector<float> &interpolated, 
         vector<float> data, 
         int width);
 
+    // Takes an vector interpolated,
+    // a plotting height, height, and
+    // a minimum and maximum to plot,
+    // min and max, and scales the 
+    // interpolation to values
+    // which can be plotted.
+    // Returns 0 on success.
+    int rasterize(
+        vector<float> &rasterized,
+        vector<float> interpolated, 
+        int height, 
+        float min, float max);
+
+    // Takes a vector, rasterized,
+    // and a given width and height,
+    // and fills up a vector of wstrings
+    // with the character set according
+    // to those values
+    // Returns 0 on success.
     int plot(
         vector<wstring> &buffer,
-        vector<float> toPlot,
+        vector<float> rasterized,
         int width, int height);
 
-    int rasterize(
-        vector<float> &toGraph,
-        vector<float> interpolate, int height, float min, float max);
 
+    // The plotting charset, defined
+    // statically through useUnicode
     static wstring m_charset;
 };
 
