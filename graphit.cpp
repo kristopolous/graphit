c57320de8b11a1bea1ab7499130758de61985e42
Chris McKenzie <kristopolous@yahoo.com>
2015-02-15 18:11:30 -0800

    formatting update

--- graphit.cpp
+++ graphit.cpp
@@ -3,53 +3,68 @@
 
 wstring Graphit::m_charset = L"";
 
-Graphit::Graphit() {
-  if(m_charset.length() == 0) {
+Graphit::Graphit() 
+{
+  if(m_charset.length() == 0) 
+  {
     Graphit::use_unicode(true);
   }
 }
 
-bool Graphit::use_unicode(bool value) {
-  if(value) {
+bool Graphit::use_unicode(bool value) 
+{
+  if(value) 
+  {
     m_charset = L" \u2581\u2582\u2583\u2584\u2585\u2586\u2587\u2588";
-  } else {
+  } 
+  else 
+  {
     m_charset = L" _.-~'`";
   }
   return value;
 }
 
-int Graphit::interpolate(vector<float>&interpolated, vector<float> data, int width ) {
+int8_t Graphit::interpolate(vector<float>&interpolated, vector<float> data, int32_t width) 
+{
   float step = (float)data.size() / width;
 
-  // first we reduce the number of items down to the number
+  //
+  // First we reduce the number of items down to the number
   // of things we are going to graph. (the width).
-  
+  // 
   // For our use-case we'll do linear interpolation for going past
   // or resolution point and one of a variety of methods when
   // going the other way.
-
+  //
   // we are fitting x units into y space
+  //
   float nextval, lhs;
   float iy = 0.0, pos = 0.0, goal = step;
 
-  for(int ix = 0; ix < data.size();) {
+  for(size_t ix = 0; ix < data.size();) 
+  {
     nextval = 0.0;
 
-    for(; (iy + 1) < goal; iy++) {
+    for(; (iy + 1) < goal; iy++) 
+    {
       nextval += data[ix];
       ix++;
     }
     // and this point we've gone through all the 
     // integer values to interpolate ... we may have
     // a fractional value left.
-    if(goal > iy) {
-      if((int)goal > (int)iy) {
+    if(goal > iy) 
+    {
+      if((int)goal > (int)iy) 
+      {
         lhs = (int)goal - iy;
         nextval += lhs * data[ix];
 
         ix++;
         nextval += (goal - iy - lhs) * data[ix];
-      } else {
+      } 
+      else 
+      {
         // ix is already where we want it to be.
         nextval += (goal - iy) * data[ix];
       }
@@ -64,55 +79,53 @@ int Graphit::interpolate(vector<float>&interpolated, vector<float> data, int wid
   return 0;
 }
 
-int Graphit::rasterize(
-    vector<float> &rasterized,
-    vector<float> interpolated, 
-    int height, float min, float max) {
+int8_t Graphit::rasterize(vector<float> &rasterized, vector<float> interpolated, int32_t height, float min, float max) 
+{
+  size_t m_charlen = Graphit::m_charset.size() - 1;
 
-  int m_charlen = Graphit::m_charset.size() - 1;
   float delta = (max - min);
 
   // now we have to find out what the graph height should be
   // this is basically value - min / (max - min)
   vector<float>::const_iterator it;
   
-  for(it = interpolated.begin(); it != interpolated.end(); it++) {
-    rasterized.push_back( (int)(m_charlen * height * ((*it - min) / delta)));
+  for(it = interpolated.begin(); it != interpolated.end(); it++) 
+  {
+    rasterized.push_back( (int32_t)(m_charlen * height * ((*it - min) / delta)));
   }
 
   return 0;
 }
 
-int Graphit::plot(
-    vector<wstring> &buffer,
-    vector<float> rasterized, 
-    int width, int height) {
+int8_t Graphit::plot(vector<wstring> &buffer, vector<float> rasterized, int32_t width, int32_t height) 
+{
+  size_t m_charlen = m_charset.size() - 1;
 
-  int m_charlen = m_charset.size() - 1;
-
-  int 
-    row_ix,
-    col_ix,
-    tmp, 
-    current_floor, 
-    current_ceiling;
+  int32_t row_ix, col_ix, tmp, current_floor, current_ceiling;
 
   wstring col;
   
-  for(row_ix = 0; row_ix < height; row_ix++) {
+  for(row_ix = 0; row_ix < height; row_ix++) 
+  {
     col.clear();
 
     // if our value is less than this then it's a space.
     current_floor = (height - row_ix - 1) * m_charlen;
     current_ceiling = current_floor + m_charlen;
 
-    for(col_ix = 0; col_ix < width; col_ix++) {
+    for(col_ix = 0; col_ix < width; col_ix++) 
+    {
       tmp = rasterized[col_ix];
-      if(tmp <= current_floor) {
+      if(tmp <= current_floor) 
+      {
         tmp = 0;
-      } else if (tmp >= current_ceiling) {
+      } 
+      else if (tmp >= current_ceiling) 
+      {
         tmp = m_charlen;
-      } else {
+      } 
+      else 
+      {
         tmp %= m_charlen;
       }
       col += m_charset[ tmp ];
@@ -124,15 +137,12 @@ int Graphit::plot(
   return 0;
 }
 
-int Graphit::process(
-  vector<wstring> &output,
-  vector<float> data, 
-  int width, int height,
-  float min, float max
-) {
-  int ret = 0;
+int8_t Graphit::process(vector<wstring> &output, vector<float> data, int32_t width, int32_t height, float min, float max) 
+{
+  int8_t ret = 0;
 
-  if(min > max || width <= 0 || height <= 0) {
+  if(min > max || width <= 0 || height <= 0) 
+  {
     return -1;
   }
 
@@ -140,84 +150,71 @@ int Graphit::process(
   vector<float> toPlot;
 
   ret = this->interpolate( interpolation, data, width);
-  if(ret) {
+  if(ret) 
+  {
     return ret;
   }
 
   ret = this->rasterize( toPlot, interpolation, height, min, max);
-  if(ret) {
+  if(ret) 
+  {
     return ret;
   }
 
   ret = this->plot( output, toPlot, width, height);
-  if(ret) {
+  if(ret) 
+  {
     return ret;
   }
 
   return ret;
 }
 
-int Graphit::print_curses(
-    vector<float> data, 
-    WINDOW*win,
-    float min, float max
-) {
-  int ret = 0;
+int8_t Graphit::print_curses(vector<float> data, WINDOW*win, float min, float max) 
+{
+  int8_t ret = 0;
 
   // let's get the width and height from an ncurses macro
-  if(!win || min > max) {
+  if(!win || min > max) 
+  {
     return -1;
   }
 
-  int width, height;
+  int32_t width, height;
   getmaxyx(win, height, width);
 
   vector<wstring> output;
-  ret = this->process(
-      output, data,
-      width, height,
-      min, max
-  );
+  ret = this->process( output, data, width, height, min, max);
 
-  if(ret) {
+  if(ret) 
+  {
     return ret;
   }
   
-  int row = 0;
-  vector<wstring>::const_iterator it;
-  for(it = output.begin(); it != output.end(); it++) {
-    mvwaddnwstr(
-      win,
-      row++, 0,
-      (const wchar_t *)((*it).c_str()), 
-      (*it).size()
-    );
+  int32_t row = 0;
+  for(vector<wstring>::const_iterator it = output.begin(); it != output.end(); it++) 
+  {
+    mvwaddnwstr(win, row++, 0, (const wchar_t *)((*it).c_str()), (*it).size());
   }
 
   wrefresh(win); 
   return ret;
 }
 
-int Graphit::print_direct(
-    vector<float> data, 
-    int width, int height,
-    float min, float max
-) {
-  int ret = 0;
+int8_t Graphit::print_direct(vector<float> data, int32_t width, int32_t height, float min, float max) 
+{
+  int8_t ret = 0;
 
   vector<wstring> output;
-  ret = this->process(
-      output, data,
-      width, height,
-      min, max
-  );
+  ret = this->process(output, data, width, height, min, max);
 
-  if(ret) {
+  if(ret) 
+  {
     return ret;
   }
 
-  vector<wstring>::const_iterator it;
-  for(it = output.begin(); it != output.end(); it++) {
+  for(vector<wstring>::const_iterator it = output.begin(); it != output.end(); it++) 
+  {
     wcout << (*it) << endl;
   }
 
--- graphit.h
+++ graphit.h
@@ -1,41 +1,44 @@
 #include <vector>
 #include <ncursesw/ncurses.h>
+#include <stdint.h>
 
 #ifndef __d8b39cb3_015d_4087_a042_d2c7d98f5eba_GRAPHIT
 #define __d8b39cb3_015d_4087_a042_d2c7d98f5eba_GRAPHIT
 
 using namespace std;
 
-class Graphit {
+class Graphit 
+{
   public:
     Graphit();
 
+    //
     // Toggles on or off unicode characters
     // Returns whatever bool you give it.
+    //
     static bool use_unicode(bool value);
 
+    //
     // Takes a vector of floats, data, and renders them
     // inside of a curses window, win, plotting between
     // Y values min and max.
     //
     // Returns 0 on success.
-    int print_curses(
-        vector<float> data, 
-        WINDOW *win,
-        float min, float max);
+    //
+    int8_t print_curses(vector<float> data, WINDOW *win, float min, float max);
 
+    //
     // Identical to print_curses but prints the 
     // results directly to the screen via wcout 
     // calls.
     //
     // Returns 0 on success.
-    int print_direct(
-        vector<float> data, 
-        int width, int height,
-        float min, float max);
+    // 
+    int8_t print_direct(vector<float> data, int32_t width, int32_t height, float min, float max);
 
   private:
 
+    //
     // Process will take a vector of floats,
     // data, and then do the following 
     // sequentially with respect to the width
@@ -53,23 +56,20 @@ class Graphit {
     // is needed to be interacted with directly.
     //
     // Returns 0 on success.
-    int process(
-        vector<wstring> &output,
-        vector<float> data, 
-        int width, int height,
-        float min, float max);
+    //
+    int8_t process(vector<wstring> &output, vector<float> data, int32_t width, int32_t height, float min, float max); 
 
+    //
     // Takes the vector, data, and does
     // a linear interpolation of it to
     // a width, width, into a vector
     // of floats, interpolated.  
     //
     // Returns 0 on success.
-    int interpolate(
-        vector<float> &interpolated, 
-        vector<float> data, 
-        int width);
+    //
+    int8_t interpolate(vector<float> &interpolated, vector<float> data, int32_t width);
 
+    //
     // Takes a vector of floats, 
     // interpolated, a plotting height, 
     // height, and a minimum and 
@@ -80,12 +80,10 @@ class Graphit {
     // of floats, rasterized.
     //
     // Returns 0 on success.
-    int rasterize(
-        vector<float> &rasterized,
-        vector<float> interpolated, 
-        int height, 
-        float min, float max);
+    //
+    int8_t rasterize(vector<float> &rasterized, vector<float> interpolated, int32_t height, float min, float max);
 
+    //
     // Takes a vector, rasterized,
     // and a given width and height,
     // and fills up a vector of wstrings,
@@ -94,14 +92,13 @@ class Graphit {
     // according to those values.
     //
     // Returns 0 on success.
-    int plot(
-        vector<wstring> &buffer,
-        vector<float> rasterized,
-        int width, int height);
+    //
+    int8_t plot(vector<wstring> &buffer, vector<float> rasterized, int32_t width, int32_t height);
 
-
+    //
     // The plotting charset, defined
     // statically through use_unicode()
+    //
     static wstring m_charset;
 };
 
